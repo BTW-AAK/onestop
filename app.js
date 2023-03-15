9e772de2563de9cf73580292e76742b11f3d02c7
BTW-AAK <aditamitkumar@gmail.com>
2023-03-15 21:47:03 +0530

    Changed Chat functionality
    Changed chat api from Comet Chat to Talk.js
    Tried to fix Email Login input field none (unresolved)

--- app.js
+++ app.js
@@ -66,11 +66,11 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   
     }
-    
+
 if (loginButton){
   loginButton.addEventListener('click', function(event) {
     event.preventDefault(); 
-    console.log("button was clicked")
+    console.log("button was clicked") 
     var emailL = document.getElementById("#emailL");
     var passwordL = document.getElementById("#passwordL");
     console.log(document.getElementById("#emailL"))
@@ -90,9 +90,50 @@ if (loginButton){
   alert(error)
 });
   })
-
 }
-     
 
+Talk.ready.then(function () {
+  var currentUserID = firebase.auth().currentUser.uid;
+  var currentUserRef = firebase.database().ref("users/" + currentUserID);
+  currentUserRef.on("value", function(snapshot) {
+    // Get the current user's name from Firebase
+    var currentUserName = snapshot.val().name;
     
+    // Set the TalkJS user's name to the current user's name
+    var me = window.talkSession.me;
+    me.name = currentUserName;
+    window.talkSession.updateUser(me);
   });
+
+  var me = new Talk.User({
+    id: 'currentUserID',
+    name: 'Alice',
+    email: 'alice@example.com',
+    photoUrl: 'https://talkjs.com/images/avatar-1.jpg',
+    welcomeMessage: 'Hey there! How are you? :-)',
+  });
+  window.talkSession = new Talk.Session({
+    appId: 'tvx8KZAs',
+    me: me,
+  });
+  var other = new Talk.User({
+    id: '21322',
+    name: 'Sebastian',
+    email: 'Sebastian@example.com',
+    photoUrl: 'https://talkjs.com/images/avatar-5.jpg',
+    welcomeMessage: 'Hey, how can I help?',
+  });
+
+  var conversation = talkSession.getOrCreateConversation(
+    Talk.oneOnOneId(me, other)
+  );
+  conversation.setParticipant(me);
+  conversation.setParticipant(other);
+
+  var inbox = talkSession.createInbox({ selected: conversation });
+  inbox.mount(document.getElementById('talkjs-container'));
+});
+
+
+});
+
